598bf5d6660c4199dd34c8a7078dae4ea09d0c2d
Sameer <sameer.marathe16536@sakec.ac.in>
2024-06-29 10:20:43 +0530

    routing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,42 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL : string
+  }
+}>()
+
+app.post('/api/v1/signup', async(c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const body = await c.req.json()
+  
+  //Check if user exists:
+  const user = await prisma.user.findUnique({
+    where:{
+      email:body.email
+    }
+  })
+  if(user){
+    return c.json({error:'User already exists'})
+  }
+
+//Else create a new user
+  await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.password,
+      name:body.name,
+
+    }
+  })
+
   return c.text('Hello Hono!')
 })
 
